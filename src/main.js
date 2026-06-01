ca4f47b9ba068a924fe906999de5e5628e032f8b
senamike388 <senamike388@gmail.com>
2026-06-01 16:12:59 +0100

    updatefiles

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-import router from './router/index.js'
-import i18n from './i18n.js'
-createApp(App).use(router).use(i18n).mount('#app')
+import router from './router'
+
+createApp(App).use(router).mount('#app')
